b4c4b83c4807c71f5369afe3a6da99f31799b861
Filippo Foglia <filippo.foglia@gmail.com>
2022-12-06 18:26:22 +0100

    CoreServiceTemplate.Security is deleted

--- CoreServicesTemplate.Security/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CoreServicesTemplate.Security.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
